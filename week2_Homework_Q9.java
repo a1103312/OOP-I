bcae92cacb3cfc91a745f6124e37cde28c33f72b
a1103312 <125727836+a1103312@users.noreply.github.com>
2023-02-26 12:55:43 +0800

    Add files via upload

---
+++ week2_Homework_Q9.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class week2_Homework_Q9{
+     public static void main(String[] args){
+            System.out.printf("Please enter an integer : ");
+            Scanner input = new Scanner(System.in);
+            int x = input.nextInt();
+            if(x==0){
+               System.out.printf(" \"0\" 雖為整數,卻非奇數也非偶數");
+            }else if(x%2==0){
+               System.out.printf("您輸入的值\"%d\"為偶數",x);
+            }else{
+               System.out.printf("您輸入的值\"%d\"為奇數",x);
+            }
+     }
+}
