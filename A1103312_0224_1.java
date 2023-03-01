1029072ea6b18be55b2c8e535933a164910bee05
a1103312 <125727836+a1103312@users.noreply.github.com>
2023-03-01 20:04:39 +0800

    Update and rename week2_Homework_Q9.java to A1103312_0224_1.java

--- A1103312_0224_1.java
+++ A1103312_0224_1.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class week2_Homework_Q9{
+public class A1103312_0224_1{
      public static void main(String[] args){
             System.out.printf("Please enter an integer : ");
             Scanner input = new Scanner(System.in);
