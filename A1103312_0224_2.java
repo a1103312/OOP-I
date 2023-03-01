823e39bb34913007c84fa8a69b1758b041f51bd0
a1103312 <125727836+a1103312@users.noreply.github.com>
2023-03-01 20:03:48 +0800

    Update and rename week2_Homework_Q10.java to A1103312_0224_2.java

--- A1103312_0224_2.java
+++ A1103312_0224_2.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class week2_Homework_Q10{
+public class A1103312_0224_2{
      public static void main(String[] args){
             System.out.printf("Please enter the degrees(Celsius) : ");
             Scanner input = new Scanner(System.in);
